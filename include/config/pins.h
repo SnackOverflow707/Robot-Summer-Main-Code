eef23f99196df09a9261b6aab65636ef5908ceda
Kenneth <ken.manohar23@gmail.com>
2026-07-14 15:03:40 -0700

    pin changes for strafing

--- include/config/pins.h
+++ include/config/pins.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#define FL_A 40 //40
-#define FL_B 21 //15
+#define FL_A 18 //40
+#define FL_B 17 //15
 
 #define FR_A 15 //16
 #define FR_B 16 //15
 
-#define BL_A 18 //18
-#define BL_B 17 //17
+#define BL_A 40 //18
+#define BL_B 21 //17
 
 #define BR_A 41 //42
 #define BR_B 42 //41
