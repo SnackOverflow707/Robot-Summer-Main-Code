794c259da46ea166b3ef515b189517a93dd591cd
SnackOverflow707 <131423796+SnackOverflow707@users.noreply.github.com>
2026-07-23 16:11:31 -0700

    fixed pins

--- include/config/pins.h
+++ include/config/pins.h
@@ -1,19 +1,19 @@
 #pragma once
 
-#define FL_A 2 //40
-#define FL_B 1 //15
+#define FL_A 1 //40
+#define FL_B 2 //15
 
 #define FR_A 5 //16
 #define FR_B 6 //15
 
-#define BL_A 3 //18
-#define BL_B 4 //17
+#define BL_A 7 //18
+#define BL_B 8 //17
 
-#define BR_A 7 //42
-#define BR_B 8 //41
+#define BR_A 3 //42
+#define BR_B 4 //41
 
 //UART link 
-#define SENSOR_UART_TX 4
-#define SENSOR_UART_RX 5
+#define SENSOR_UART_TX 17
+#define SENSOR_UART_RX 16
 
 #define FREQ_SWITCH_PIN 11
