09bfbbcfa12c043947370e190fe4a43aaeb74193
Annabel <li.annabel47@gmail.com>
2026-07-28 20:33:05 -0700

    testing new tower angles

--- src/robotArm/armSequences/tower.h
+++ src/robotArm/armSequences/tower.h
@@ -5,19 +5,20 @@
 #include "../taskManager.h"
 
 #define TOWERS_TO_ATTEMPT 3
+#define TOWER_CLAW_CLOSED 43
 
 //tower positions 
-static const ArmPose REACH_TOWER_1 = {155, 57, 210, 60, 20}; 
-static const ArmPose GRAB_TOWER_1 = {155, 57, 210, 60, 45}; 
-static const ArmPose REACH_TOWER_2 = {170, 45, 180, 35, 0}; 
-static const ArmPose GRAB_TOWER_2 = {170, 45, 180, 35, 45}; 
-static const ArmPose REACH_TOWER_3 = {180, 38, 168, 55, 0}; 
-static const ArmPose GRAB_TOWER_3 = {180, 38, 168, 55, 45}; ; 
+static const ArmPose REACH_TOWER_1 = {155, 170, 235, 70, 20}; 
+static const ArmPose GRAB_TOWER_1 = {155, 170, 235, 70, TOWER_CLAW_CLOSED}; 
+static const ArmPose REACH_TOWER_2 = {160, 175, 220, 65, 0}; 
+static const ArmPose GRAB_TOWER_2 = {160, 175, 220, 65, TOWER_CLAW_CLOSED}; 
+static const ArmPose REACH_TOWER_3 = {175, 195, 170, 25, 0}; 
+static const ArmPose GRAB_TOWER_3 = {175, 195, 170, 25, TOWER_CLAW_CLOSED}; 
 
 
 const char* pickupOrder[] = {
-    "base",
     "shoulder",
+    "base",
     "wrist",
     "elbow",
     "claw"
@@ -35,10 +36,10 @@ static const std::vector<ArmPose> ALL_TOWERS = {
 
 //repeat positions 
 static const ArmPose ORIENT = {HOME_BASE, HOME_SHOULDER, HOME_ELBOW, 90, HOME_CLAW}; //base turns 90deg, claw is open
-static const ArmPose RETRACT = {160, 120, 180, 60, 45}; 
-static const ArmPose FUNNEL1 = {0, 120, 180, 60, 45}; 
-static const ArmPose FUNNEL2 = {0, 120, 180, 35, 45}; 
-static const ArmPose DROP_TOWER = {0, 120, 180, 35, 0};  
+static const ArmPose RETRACT = {160, 120, 180, 60, 43}; 
+static const ArmPose FUNNEL1 = {0, 95, 220, 70, 43}; 
+static const ArmPose FUNNEL2 = {0, 95, 230, 70, 43}; //version 2: 0, 85, 235, 60, open/closed
+static const ArmPose DROP_TOWER = {0, 95, 230, 70, 0};  
 
 //repeat sequence
 static const std::vector<ArmPose> TOWER_DROP_IN_FUNNEL = {
