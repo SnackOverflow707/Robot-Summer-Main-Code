dc4b3822acf6be6041b5532d6ff95ebb33bab3ee
SnackOverflow707 <131423796+SnackOverflow707@users.noreply.github.com>
2026-07-31 18:22:20 -0700

    fix some solar panel stuff

--- src/robotArm/armSequences/solarPanels.h
+++ src/robotArm/armSequences/solarPanels.h
@@ -7,8 +7,7 @@
 #define CLAW_CLOSED_PANEL 43
 
 //positions to reach the solar panels
-static const ArmPose PRE_REACH_PANEL = {40, 165, 270, 190, 0}; 
-static const ArmPose REACH_PANEL = {40, 165, 270, 190, 35}; 
+static const ArmPose REACH_PANEL = {40, 165, 270, 190, 10}; 
 static const ArmPose GRAB_PANEL = {40, 165, 270, 190, CLAW_CLOSED_PANEL}; //update after testing
 static const ArmPose RECENTER = {135, 90, 270, 190, CLAW_CLOSED_PANEL};
 
@@ -16,7 +15,6 @@ const char* reachPanelOrder[] = { "claw", "shoulder", "elbow", "wrist", "base" }
 const char* recenterOrder[] = {"elbow", "wrist", "base", "shoulder", "claw"};
 
 inline void solarPanelSequence(TaskManager& taskManager) {
-    taskManager.executeMove(PRE_REACH_PANEL, reachPanelOrder);
     taskManager.executeMove(REACH_PANEL, reachPanelOrder); 
     delay(250); 
     taskManager.executeMove(GRAB_PANEL); 
