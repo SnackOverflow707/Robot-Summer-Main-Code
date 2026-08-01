0bddf3793b648a264264c487f05132f85d5930f9
Annabel <li.annabel47@gmail.com>
2026-07-31 17:58:53 -0700

    adding joint movement orders

--- src/robotArm/armSequences/solarPanels.h
+++ src/robotArm/armSequences/solarPanels.h
@@ -7,17 +7,21 @@
 #define CLAW_CLOSED_PANEL 43
 
 //positions to reach the solar panels
+static const ArmPose PRE_REACH_PANEL = {40, 165, 270, 190, 0}; 
 static const ArmPose REACH_PANEL = {40, 165, 270, 190, 35}; 
 static const ArmPose GRAB_PANEL = {40, 165, 270, 190, CLAW_CLOSED_PANEL}; //update after testing
 static const ArmPose RECENTER = {135, 90, 270, 190, CLAW_CLOSED_PANEL};
 
-inline void solarPanelSequence(TaskManager& taskManager) {
+const char* reachPanelOrder[] = { "claw", "shoulder", "elbow", "wrist", "base" };
+const char* recenterOrder[] = {"elbow", "wrist", "base", "shoulder", "claw"};
 
-    taskManager.executeMove(REACH_PANEL); 
+inline void solarPanelSequence(TaskManager& taskManager) {
+    taskManager.executeMove(PRE_REACH_PANEL, reachPanelOrder);
+    taskManager.executeMove(REACH_PANEL, reachPanelOrder); 
     delay(250); 
     taskManager.executeMove(GRAB_PANEL); 
     delay(400); 
-    taskManager.executeMove(RECENTER); 
+    taskManager.executeMove(RECENTER, recenterOrder); 
 
 }
 
