1198c9477aa058df179671bdaee4df3fd5b50a06
Annabel <li.annabel47@gmail.com>
2026-08-05 17:29:14 -0700

    dummy angles

--- src/robotArm/armSequences/solarPanels.h
+++ src/robotArm/armSequences/solarPanels.h
@@ -8,7 +8,7 @@
 
 //positions to reach the solar panels
 static const ArmPose REACH_PANEL_1k = {40, 155, 270, 165, 10}; 
-static const ArmPose GRAB_PANEL_1k = {40, 155, 270, 165, CLAW_CLOSED_PANEL}; //update after testing
+static const ArmPose GRAB_PANEL_1k = {40, 155, 270, 165, CLAW_CLOSED_PANEL}; 
 static const ArmPose RECENTER = {135, 90, 270, 190, CLAW_CLOSED_PANEL};
 
 inline const char* reachPanelOrder[] = { "claw", "shoulder", "elbow", "wrist", "base" };
@@ -33,9 +33,10 @@ inline void solarPanelSequence_1KHz(TaskManager& taskManager) {
 // --------------------------------------------------
 
 // TODO: fill in with the angles measured for the fallback position.
-static const ArmPose REACH_PANEL_FALLBACK = {0, 0, 0, 0, 10};
-static const ArmPose GRAB_PANEL_FALLBACK = {0, 0, 0, 0, CLAW_CLOSED_PANEL};
-static const ArmPose RECENTER_FALLBACK = {0, 0, 0, 0, CLAW_CLOSED_PANEL};
+//NOT TESTED YET. 
+static const ArmPose REACH_PANEL_FALLBACK = {25, 155, 270, 165, 10};
+static const ArmPose GRAB_PANEL_FALLBACK = {25, 155, 270, 165, CLAW_CLOSED_PANEL};
+static const ArmPose RECENTER_FALLBACK = {135, 90, 270, 190, CLAW_CLOSED_PANEL};
 
 inline void solarPanelFallbackSequence_1KHz(TaskManager& taskManager) {
     taskManager.executeMove(REACH_PANEL_FALLBACK, reachPanelOrder);
