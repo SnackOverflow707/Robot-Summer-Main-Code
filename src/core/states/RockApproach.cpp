1961d896c9fda7046d79edbc8371263a4a2e9215
SnackOverflow707 <131423796+SnackOverflow707@users.noreply.github.com>
2026-08-06 02:28:02 -0700

    reduce rock positions

--- src/core/states/RockApproach.cpp
+++ src/core/states/RockApproach.cpp
@@ -11,12 +11,12 @@ namespace RockApproach
 
         
     const RockPos ROCK_POSITIONS[6] = {
-        {  55.7328f,  -604.2605f, 1, true  },  // Rock 1
-        { -19.4728f, -1126.4563f, 0, true  },  // Rock 2
-        {  89.8105f, -1405.8289f, 1, false },  // Rock 3
-        { 335.1521f, -1755.6941f, 1, false },  // Rock 4
-        { 354.2891f, -3393.1648f, 0, false },  // Rock 5
-        { 252.7281f, -3708.1218f, 1, true  }   // Rock 6
+        {  55.7328f,  -554.2605f, 1, true  },  // Rock 1
+        { -19.4728f, -1026.4563f, 0, true  },  // Rock 2
+        {  89.8105f, -1305.8289f, 1, false },  // Rock 3
+        { 335.1521f, -1655.6941f, 1, false },  // Rock 4
+        { 354.2891f, -3293.1648f, 0, false },  // Rock 5
+        { 252.7281f, -3608.1218f, 1, true  }   // Rock 6
     };
     struct ScanPos {
         float x;
